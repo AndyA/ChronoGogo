44fd52fa14a84d8d57f74f40dd04a88f93381316
Andy Armstrong <andy@hexten.net>
2017-07-06 23:37:24 +0100

    Different modes.

--- chrono.cpp
+++ chrono.cpp
@@ -60,6 +60,21 @@ static void timeCone(Mat &m, Size sz, double cxp, double cyp, int channel) {
   }
 }
 
+static void timeRand(Mat &m, Size sz, int channel) {
+  m.create(sz, CV_8UC3);
+
+  int channels = m.channels();
+  CV_Assert(channel >= 0 && channel < channels);
+
+  for (int y = 0; y < sz.height; y++) {
+    uchar *row = m.ptr<uchar>(y);
+    for (int x = 0; x < sz.width; x++) {
+      double r = (double) rand() / RAND_MAX;
+      row[x * channels + channel] = (unsigned char) HISTORY * r;
+    }
+  }
+}
+
 static void timeBend(Mat &out, FrameStore &fs, const Mat &timeMap) {
   Mat *fr = fs.history(0);
   if (!fr) return;
@@ -103,6 +118,31 @@ static void timeBend(Mat &out, FrameStore &fs, const Mat &timeMap) {
   }
 }
 
+static void setMap(Mat &timeMap, const Mat &frame, int which) {
+  switch (which) {
+  default:
+  case '0':
+    for (int c = 0; c < 3; c++) {
+      timeCone(timeMap, frame.size(), 0.5, 0.5, c);
+    }
+    break;
+
+  case '1':
+    for (int c = 0; c < 3; c++) {
+      double cx = sin(M_PI * 2 / 3 * c) * 0.1 + 0.5;
+      double cy = cos(M_PI * 2 / 3 * c) * 0.1 + 0.5;
+      timeCone(timeMap, frame.size(), cx, cy, c);
+    }
+    break;
+
+  case '2':
+    for (int c = 0; c < 3; c++) {
+      timeRand(timeMap, frame.size(), c);
+    }
+    break;
+  }
+}
+
 int main(int, char **) {
   FrameStore fs(HISTORY);
   VideoCapture cap(0);
@@ -117,13 +157,10 @@ int main(int, char **) {
 
   Mat chrono, timeMap;
   Mat *fr = fs.next();
+
   cap >> *fr;
 
-  for (int c = 0; c < 3; c++) {
-    double cx = sin(M_PI * 2 / 3 * c) * 0.1 + 0.5;
-    double cy = cos(M_PI * 2 / 3 * c) * 0.1 + 0.5;
-    timeCone(timeMap, fr->size(), cx, cy, c);
-  }
+  setMap(timeMap, *fr, '1');
 
   for (;;) {
     flip(*fr, *fr, 1);
@@ -140,7 +177,13 @@ int main(int, char **) {
     }
     imshow("chrono", chrono);
 
-    waitKey(10);
+    int k = waitKey(10);
+
+    if (k == 'Q' || k == 'q')
+      break;
+
+    if (k >= '0' && k <= '9')
+      setMap(timeMap, *fr, k);
 
     fr = fs.next();
     cap >> *fr;
